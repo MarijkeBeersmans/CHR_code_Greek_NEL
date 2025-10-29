1e28e801c62aa917aefa1a4c9d70814092fd24c0
u0161477 <u0161477@kuleuven.be>
2025-10-29 14:35:00 +0100

    add large file download support + final test

--- blink_training/CHR_testing.sh
+++ blink_training/CHR_testing.sh
@@ -3,12 +3,13 @@
 
 data_version=v6
 
-for data_quality in gold gold_silver
+for data_quality in full manual
 do
-    for text_length in voll kurz
+    for text_length in kurz voll
     do
         echo "Running for: $data_quality $text_length"
-        bash test_BLINK_model.sh models/$data_quality_$text_length $text_length test manual
+        bash test_BLINK_model.sh blink_CHR_v2_${text_length}_${data_quality} $text_length test manual
         mkdir -p ../test_results/${text_length}_${data_quality}
-        mv /models/${text_length}_${data_quality}/logs/* ../test_results/${text_length}_${data_quality}/
+        mv ../models/${text_length}_${data_quality}/logs/* ../test_results/${text_length}_${data_quality}/
+    done
 done
--- download_models.py
+++ download_models.py
@@ -1,17 +1,20 @@
-import os, requests, zipfile
+import os, requests, zipfile, tqdm
 
 base_url = "https://zenodo.org/records/17463358/files"
-models = ["kurz_gold.zip", "kurz_gold_silver.zip", "voll_gold.zip", "voll_gold_silver.zip"]
+#models = ["kurz_gold_silver.zip","kurz_gold.zip", "voll_gold.zip", "voll_gold_silver.zip"]
+models = ["kurz_gold_silver.zip"]
 
 os.makedirs("models", exist_ok=True)
 
 for m in models:
     url = f"{base_url}/{m}"
     print(f"Downloading {m}...")
-    r = requests.get(url)
-    r.raise_for_status()
-    with open(f"models/{m}", "wb") as f:
-        f.write(r.content)
+    with requests.get(url, stream=True, timeout=300) as r:
+        r.raise_for_status()
+        with open(f'models/{m}', "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
     with zipfile.ZipFile(f"models/{m}") as z:
         z.extractall("models/")
 
